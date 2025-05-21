696f6ef070521766f3ff9353da1c045990ebe7d0
Will-Swinson <william.swinson01@gmail.com>
2025-05-21 03:42:26 -0500

    Normal Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-rose-500">Hello Everyone</p>
+      <Button variant="link">Hello World</Button>
+    </div>
+  );
+}
