6e012ff34f3b0cb8c973024e30938c234a48b422
Will-Swinson <william.swinson01@gmail.com>
2025-05-22 13:17:03 -0500

    Style Customization Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">Hello Everyone</p>
-      <Button variant="link">Hello World</Button>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">I AM A BUTTON</Button>
+        </div>
+        <div>
+          <Input placeholder="I AM A INPUT" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea value="I AM A TEXTAREA" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
