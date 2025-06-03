cf1264f1b34cefc5f2265bfffad7af2c1baaacc4
Will-Swinson <william.swinson01@gmail.com>
2025-06-03 09:51:25 -0500

    added authentication & login / signup pages

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,5 +1,12 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  
-  return <div className="p-4"></div>;
+  const trpc = useTRPC();
+
+  const { data } = useQuery(trpc.auth.session.queryOptions());
+  console.log(data);
+  return <div className="p-4">{JSON.stringify(data?.user, null, 2)}</div>;
 }
---
+++ src/modules/auth/server/procedures.ts
@@ -0,0 +1,96 @@
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
+import { headers as getHeaders, cookies as getCookies } from "next/headers";
+import { AUTH_COOKIE } from "../contants";
+import { loginSchema, registerSchema } from "../schemas";
+export const authRouter = createTRPCRouter({
+  session: baseProcedure.query(async ({ ctx }) => {
+    const headers = await getHeaders();
+    const session = await ctx.payload.auth({ headers });
+    console.log(session);
+    return session;
+  }),
+  register: baseProcedure
+    .input(registerSchema)
+    .mutation(async ({ input, ctx }) => {
+      const existingData = await ctx.payload.find({
+        collection: "users",
+        limit: 1,
+        where: {
+          username: {
+            equals: input.username,
+          },
+        },
+      });
+
+      const existingUsername = existingData.docs[0];
+
+      if (existingUsername)
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Username already taken",
+        });
+
+      await ctx.payload.create({
+        collection: "users",
+        data: {
+          email: input.email,
+          username: input.username,
+          password: input.password,
+        },
+      });
+
+      const data = await ctx.payload.login({
+        collection: "users",
+        data: {
+          email: input.email,
+          password: input.password,
+        },
+      });
+
+      if (!data.token) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "Failed to login",
+        });
+      }
+
+      const cookies = await getCookies();
+      cookies.set({
+        name: AUTH_COOKIE,
+        value: data.token,
+        httpOnly: true,
+        path: "/",
+      });
+    }),
+  login: baseProcedure.input(loginSchema).mutation(async ({ input, ctx }) => {
+    const data = await ctx.payload.login({
+      collection: "users",
+      data: {
+        email: input.email,
+        password: input.password,
+      },
+    });
+
+    if (!data.token) {
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+        message: "Failed to login",
+      });
+    }
+
+    const cookies = await getCookies();
+    cookies.set({
+      name: AUTH_COOKIE,
+      value: data.token,
+      httpOnly: true,
+      path: "/",
+    });
+
+    return data;
+  }),
+  logout: baseProcedure.mutation(async () => {
+    const cookies = await getCookies();
+    cookies.delete(AUTH_COOKIE);
+  }),
+});
