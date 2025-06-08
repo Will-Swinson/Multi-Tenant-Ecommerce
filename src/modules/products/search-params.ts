f3550d0459e04a70d5d6a4329bb25e10643acb11
Will-Swinson <william.swinson01@gmail.com>
2025-06-08 08:15:18 -0500

    added filtering and sorting functionality and hooked it up to the api

--- src/modules/products/search-params.ts
+++ src/modules/products/search-params.ts
@@ -1,11 +1,11 @@
 import {
-  useQueryStates,
+  createLoader,
   parseAsArrayOf,
   parseAsString,
   parseAsStringLiteral,
-} from "nuqs";
+} from "nuqs/server";
 
-const sortValues = ["curated", "trending", "hot_and_new"] as const;
+export const sortValues = ["curated", "trending", "hot_and_new"] as const;
 
 export const params = {
   sort: parseAsStringLiteral(sortValues).withDefault("curated"),
@@ -25,6 +25,5 @@ export const params = {
     })
     .withDefault([]),
 };
-export default function useProductFilters() {
-  return useQueryStates(params);
-}
+
+export const loadProductFilters = createLoader(params);
