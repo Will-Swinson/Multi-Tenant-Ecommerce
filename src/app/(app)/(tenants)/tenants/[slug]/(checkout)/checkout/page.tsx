fa6273d5f2698167cec3375adecdaf1a10bccda4
Will-Swinson <william.swinson01@gmail.com>
2025-06-10 15:50:37 -0500

    added checkout page client side setup

--- src/app/(app)/(tenants)/tenants/[slug]/(checkout)/checkout/page.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(checkout)/checkout/page.tsx
@@ -1,3 +1,10 @@
-export default function CheckoutPage() {
-  return <div>page</div>;
+import CheckoutView from "@/modules/checkout/ui/views/checkout-view";
+
+interface Props {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function CheckoutPage({ params }: Props) {
+  const { slug } = await params;
+  return <CheckoutView tenantSlug={slug} />;
 }
